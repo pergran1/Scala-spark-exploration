31d9304427349adca51cdaf0869ba26c9a41d66b
pergran1 <pergranberg@hotmail.com>
2022-07-31 14:34:24 +0200

    Read csv fanfiction

---
+++ src/main/scala/Exploration/Fanfiction/KPIs_creation.scala
@@ -0,0 +1,27 @@
+package Exploration.Fanfiction
+import Exploration.Fanfiction.KPIs.spark
+import org.apache.spark.sql.SparkSession
+
+object KPIs_creation extends App{
+
+  //create the spark session
+  val spark = SparkSession.builder()
+    .appName("KPIs_Creation")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  //reading the data
+  val df = spark.read
+    .format("csv")
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .option("delimiter", ",")
+    .option("multiline", "true")
+    .option("escape", """"""") // in pyspark it was: escape = '"' which was much cleaner
+    .load("src/main/scala/Exploration/Fanfiction/data/fanfiction_data_2022-06-05.csv")
+
+  val sc = spark.sparkContext
+  sc.setLogLevel("ERROR") //less error text so easier to see
+
+  df.show()
+}
