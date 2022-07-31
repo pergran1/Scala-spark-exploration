933c6a34d64b5832f4e49d3dcf5dff086ef62fe7
pergran1 <pergranberg@hotmail.com>
2022-07-31 10:49:15 +0200

    Changes folder structure

--- src/main/scala/Exploration/Fanfiction/KPIs.scala
+++ src/main/scala/Exploration/Fanfiction/KPIs.scala
@@ -1,8 +1,9 @@
-package Fanfiction_exploration
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.types._
+package Exploration.Fanfiction
 
-object KPIs extends App{
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SparkSession}
+
+object KPIs extends App {
 
   /**
    * This creates a SparkSession, which will be used to operate on the DataFrames that we create.
@@ -37,16 +38,16 @@ object KPIs extends App{
    * A "manual" sequence of rows describing cars, fetched from cars.json in the data folder.
    */
   val cars = Seq(
-    Row("chevrolet chevelle malibu",18.0,8L,307.0,130L,3504L,12.0,"1970-01-01","USA"),
-    Row("buick skylark 320",15.0,8L,350.0,165L,3693L,11.5,"1970-01-01","USA"),
-    Row("plymouth satellite",18.0,8L,318.0,150L,3436L,11.0,"1970-01-01","USA"),
-    Row("amc rebel sst",16.0,8L,304.0,150L,3433L,12.0,"1970-01-01","USA"),
-    Row("ford torino",17.0,8L,302.0,140L,3449L,10.5,"1970-01-01","USA"),
-    Row("ford galaxie 500",15.0,8L,429.0,198L,4341L,10.0,"1970-01-01","USA"),
-    Row("chevrolet impala",14.0,8L,454.0,220L,4354L,9.0,"1970-01-01","USA"),
-    Row("plymouth fury iii",14.0,8L,440.0,215L,4312L,8.5,"1970-01-01","USA"),
-    Row("pontiac catalina",14.0,8L,455.0,225L,4425L,10.0,"1970-01-01","USA"),
-    Row("amc ambassador dpl",15.0,8L,390.0,190L,3850L,8.5,"1970-01-01","USA")
+    Row("chevrolet chevelle malibu", 18.0, 8L, 307.0, 130L, 3504L, 12.0, "1970-01-01", "USA"),
+    Row("buick skylark 320", 15.0, 8L, 350.0, 165L, 3693L, 11.5, "1970-01-01", "USA"),
+    Row("plymouth satellite", 18.0, 8L, 318.0, 150L, 3436L, 11.0, "1970-01-01", "USA"),
+    Row("amc rebel sst", 16.0, 8L, 304.0, 150L, 3433L, 12.0, "1970-01-01", "USA"),
+    Row("ford torino", 17.0, 8L, 302.0, 140L, 3449L, 10.5, "1970-01-01", "USA"),
+    Row("ford galaxie 500", 15.0, 8L, 429.0, 198L, 4341L, 10.0, "1970-01-01", "USA"),
+    Row("chevrolet impala", 14.0, 8L, 454.0, 220L, 4354L, 9.0, "1970-01-01", "USA"),
+    Row("plymouth fury iii", 14.0, 8L, 440.0, 215L, 4312L, 8.5, "1970-01-01", "USA"),
+    Row("pontiac catalina", 14.0, 8L, 455.0, 225L, 4425L, 10.0, "1970-01-01", "USA"),
+    Row("amc ambassador dpl", 15.0, 8L, 390.0, 190L, 3850L, 8.5, "1970-01-01", "USA")
   )
 
   /**
