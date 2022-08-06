a7a219348081519d2825bd6c05f662425ae9f7a2
pergran1 <pergranberg@hotmail.com>
2022-08-06 17:30:53 +0200

    Added training for different datasources

---
+++ src/main/scala/Exploration/Pyspark_training/Datasources_and_format_different.scala
@@ -0,0 +1,7 @@
+package Exploration.Pyspark_training
+
+
+
+object Datasources_and_format_different extends App{
+
+}
--- src/main/scala/Exploration/Pyspark_training/dataframe_basic.scala
+++ src/main/scala/Exploration/Pyspark_training/dataframe_basic.scala
@@ -1,4 +1,4 @@
-package Exploration.Pyspark
+package Exploration.Pyspark_training
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.{Row, SparkSession}
